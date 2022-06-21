499fbd697963fd8d1cde5bd8df62b830027204e2
Priscilla <priscillararimmy@hotmail.com>
2022-06-21 13:57:13 -0300

    feat(Experiencia): Finalizado!

--- src/components/experience/experiense.jsx
+++ src/components/experience/experiense.jsx
@@ -1,9 +1,94 @@
 import React from "react";
 import './experiense.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experiense = () => {
     return (
-        <section id="experiense">Experiense</section>
+        <section id="experiense">
+            <h2>Minhas Skills</h2>
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Frontend</h3>
+                    <div className="experience__content">
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon"/>
+                            <div>
+                            <h4>HTML</h4>
+                            <small className="text-light">Intermediário</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>CSS</h4>
+                            <small className="text-light">Intermediário</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>JavaScrip</h4>
+                            <small className="text-light">Intermediário</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>WordPress</h4>
+                            <small className="text-light">Intermediário</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>Bootstrap</h4>
+                            <small className="text-light">Intermediário</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>ReactJs</h4>
+                            <small className="text-light">Básico</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div className="experience__backend">
+                <h3>Backend</h3>
+                    <div className="experience__content">
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>NodeJs</h4>
+                            <small className="text-light">Básico</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>MySQL</h4>
+                            <small className="text-light">Básico</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon" />
+                            <div>
+                            <h4>MongoDB</h4>
+                            <small className="text-light">Básico</small>
+                            </div>
+                        </article>
+                        <article className="experiense__details">
+                            <BsFillPatchCheckFill className="experience_details-icon"/>
+                            <div>
+                            <h4>Postgress</h4>
+                            <small className="text-light">Básico</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="me">
             <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">&#8595;</a>
         </div>
        </header>
     )
