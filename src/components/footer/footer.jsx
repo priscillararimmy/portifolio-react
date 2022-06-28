1786a912e5f2bda69f8300513c57f7ef486ad828
Priscilla <priscillararimmy@hotmail.com>
2022-06-27 21:59:12 -0300

    feat(Footer):Finalização

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import './footer.css'
+import {FaGithubAlt} from 'react-icons/fa'
+import {GrInstagram} from 'react-icons/gr'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
-    <div>Footer</div>
+    return (
+    <footer>
+        <a href="#" className="footer__logo">PRISCILLA</a>
+        <ul className="permalinks">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">Sobre mim</a></li>
+            <li><a href="#experiense">Experiência</a></li>
+            <li><a href="#portifolio">Projetos</a></li>
+            <li><a href="#contact">Contato</a></li>
+        </ul>
+
+        <div className="footer__socials">
+            <a href="https://www.linkedin.com/in/priscilla-rarimmy-lopes-pereira-1174461aa/"><BsLinkedin /></a>
+            <a href="https://github.com/priscillararimmy"><FaGithubAlt /></a>
+            <a href="https://www.instagram.com/priscillararimmy/"><GrInstagram /></a>
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; Todos os direitos reservados</small>
+        </div>
+    </footer>
+    )
 }
 
 export  default Footer
